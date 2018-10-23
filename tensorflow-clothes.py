1374ddf7d179085e109fcfef425b5b8a41afb2fd
Michael Garas <michaelgaras@Michaels-MacBook-Pro.local>
2018-10-23 18:43:14 -0400

    Added branch Michael

--- tensorflow-clothes.py
+++ tensorflow-clothes.py
@@ -18,14 +18,26 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 
 print(train_images.shape)
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+
+plt.figure()
+plt.imshow(train_images[5])
+plt.colorbar()
+plt.grid(False)
+
+
+
+#plt.figure(figsize=(10,10))
+#for i in range(25):
+#    plt.subplot(5,5,i+1)
+#    plt.xticks([])
+#    plt.yticks([])
+#    plt.grid(False)
+#    plt.imshow(train_images[i], cmap=plt.cm.binary)
+#    plt.xlabel(class_names[train_labels[i]])
 
 
 plt.show()
